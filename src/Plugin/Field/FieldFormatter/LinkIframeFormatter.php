117036803d4551f4a543ef9a0e78fa71affd27dd
larowlan <larowlan@395439.no-reply.drupal.org>
2021-07-05 08:45:23 +1000

    Issue #2873610 by Munavijayalakshmi, larowlan, dhruveshdtripathi: Convert module to use short array syntax (new coding standard)

--- src/Plugin/Field/FieldFormatter/LinkIframeFormatter.php
+++ src/Plugin/Field/FieldFormatter/LinkIframeFormatter.php
@@ -23,49 +23,49 @@ class LinkIframeFormatter extends LinkFormatter {
    * {@inheritdoc}
    */
   public static function defaultSettings() {
-    return array(
+    return [
         'width' => '640',
         'height' => '480',
         'class' => '',
         'original' => '',
-    );
+    ];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(array $form, FormStateInterface $form_state)  {
+  public function settingsForm(array $form, FormStateInterface $form_state) {
 
-    $elements['width'] = array(
+    $elements['width'] = [
       '#title' => $this->t('Width'),
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('width'),
       '#required' => TRUE,
-    );
+    ];
 
-    $elements['height'] = array(
+    $elements['height'] = [
       '#title' => $this->t('Height'),
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('height'),
       '#required' => TRUE,
-    );
+    ];
 
-    $elements['class'] = array(
+    $elements['class'] = [
       '#title' => $this->t('Class'),
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('class'),
       '#required' => FALSE,
-    );
-    $elements['original'] = array(
+    ];
+    $elements['original'] = [
       '#title' => $this->t('Show original link'),
       '#type' => 'radios',
-      '#options' => array(
+      '#options' => [
         TRUE => $this->t('On'),
         FALSE => $this->t('Off'),
-        ),
+        ],
       '#default_value' => $this->getSetting('original'),
       '#required' => FALSE,
-    );
+    ];
 
     return $elements;
   }
@@ -74,8 +74,8 @@ class LinkIframeFormatter extends LinkFormatter {
    * {@inheritdoc}
    */
   public function settingsSummary() {
-    $summary = array();
-    $summary[] = $this->t('Width: @width, Height: @height, Class: @class, Original link is @original', array('@width' => $this->getSetting('width'), '@height' => $this->getSetting('height'), '@class' => $this->getSetting('class') == "" ? 'None' : $this->getSetting('class'), '@original' => $this->getSetting('original') ? t('On') : t('Off')));
+    $summary = [];
+    $summary[] = $this->t('Width: @width, Height: @height, Class: @class, Original link is @original', ['@width' => $this->getSetting('width'), '@height' => $this->getSetting('height'), '@class' => $this->getSetting('class') == "" ? 'None' : $this->getSetting('class'), '@original' => $this->getSetting('original') ? t('On') : t('Off')]);
     return $summary;
   }
 
@@ -83,14 +83,14 @@ class LinkIframeFormatter extends LinkFormatter {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $element = array();
+    $element = [];
     $settings = $this->getSettings();
 
     foreach ($items as $delta => $item) {
       // By default use the full URL as the link text.
       $url = $this->buildUrl($item);
 
-      $element[$delta] = array(
+      $element[$delta] = [
           '#theme' => 'link_iframe_formatter',
           '#url' => $url,
           '#width' => $settings['width'],
@@ -98,7 +98,7 @@ class LinkIframeFormatter extends LinkFormatter {
           '#class' => $settings['class'],
           '#original' => $settings['original'],
           '#path' => $url,
-        );
+        ];
     }
     return $element;
   }
